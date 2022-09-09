b131428e94b4fe1f763389d83530f466b7d2f975
Tamara Hovhannisyan <mycroft0>
2022-09-09 18:38:45 +0400

    registration page

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,21 +1,53 @@
 import {useForm} from "react-hook-form";
 import {useAuthRoute} from "../../contexts/RouteProvider";
 import {ACTIVE_ROUTES} from "../../helpers/constants";
+import classes from "./Login.module.css"
+import {useState} from "react";
+import axios from "axios";
+import {baseUrl} from "../api/api";
+import {useNavigate} from "react-router-dom";
 
-const [,REGISTRATION] = ACTIVE_ROUTES
+const [, REGISTRATION] = ACTIVE_ROUTES
 
 const Login = () => {
+
+    const [isAuthFailed, setIsAuthFailed] = useState(false)
     const {setActiveRoute} = useAuthRoute()
-    const {register, handleSubmit, formState:{errors}} =  useForm()
+    const {register, handleSubmit, formState: {errors}} = useForm()
+
+    const navigate = useNavigate()
+
+    const onSubmit = data => {
+        axios.get(`${baseUrl}/users`)
+            .then(res=>{
+                const user = res.data.find(user=>user.name === data.login && user.password === data.password)
+                if(user){
+                    navigate('../messages')
+                }else {
+                    console.error('User not found!')
+                    setIsAuthFailed(true)
+                }
+            })
+    }
 
     return (
         <div>
-            <form onSubmit={handleSubmit(()=>{})}>
-                <input type={'text'}/>
-                <input type={'text'}/>
+            <form className={classes.form}
+                  onSubmit={handleSubmit(onSubmit)}>
+                <label className={classes.label}>
+                    LOGIN
+                    <input {...register('login', {required: true})} type={'text'}/>
+                </label>
+                <label className={classes.label}>
+                    PASSWORD
+                    <input {...register('password', {required: true})} type={'password'}/>
+                </label>
+                <button type={"submit"}>LOG IN</button>
             </form>
-            Login
-            <button onClick={()=> setActiveRoute(REGISTRATION)}>go to registration</button>
+            {
+                isAuthFailed &&  <button onClick={() => setActiveRoute(REGISTRATION)}>go to registration</button>
+            }
+
         </div>
 
     )
--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,15 +1,55 @@
 import {useAuthRoute} from "../../contexts/RouteProvider";
 import {ACTIVE_ROUTES} from "../../helpers/constants";
+import {useForm} from "react-hook-form";
+import {useState} from "react";
+import classes from "./Register.module.css"
+import axios from "axios";
+import {baseUrl} from "../api/api";
 
-const [LOGIN, REGISTRATION] = ACTIVE_ROUTES
+const [LOGIN] = ACTIVE_ROUTES
 
 const Register = () => {
+
     const {setActiveRoute} = useAuthRoute()
+    const [isRegisterFailed, setIsRegisterFailed] = useState(true)
+    const {register, handleSubmit, formState: {errors}} = useForm()
+
+    const onSubmit = data => {
+
+        axios.post(`${baseUrl}/users`, {
+            name:data.login,
+            password: data.password
+        })
+
+
+        setIsRegisterFailed(false)
+        setTimeout(() => {
+            setActiveRoute(LOGIN)
+        }, 1000)
+    }
 
     return (
         <div>
-            Register
-            <button onClick={()=> setActiveRoute(LOGIN)}>go to login</button>
+            <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
+                <label className={classes.label}>
+                    LOGIN
+                    <input {...register('login', {required: true})} type={'text'}></input>
+                </label>
+                <label className={classes.label}>
+                    PASSWORD
+                    <input  {...register('password', {required: true})} type={'password'}></input>
+                </label>
+                <label className={classes.label}>
+                    REPEAT PASSWORD
+                    <input  {...register('rePassword', {required: true})} type={'password'}></input>
+                </label>
+                <button type={'submit'}>REGISTER</button>
+            </form>
+
+            {
+                !isRegisterFailed && <p>Registration is succeed!</p>
+            }
+
         </div>
 
     )
